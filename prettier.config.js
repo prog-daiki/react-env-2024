e3f885059f8b10c90459a04151b2b4140ee386c7
prog-daiki <t.daiki0913@icloud.com>
2024-02-01 21:44:31 +0900

    first commit

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/**@type {import("prettier").Config} */
+const config = {
+  semi: true,
+  tabWidth: 2,
+  jsxSingleQuote: true,
+  trailingComma: "all",
+  useTabs: false,
+  bracketSpacing: true,
+};
+
+export default config;
